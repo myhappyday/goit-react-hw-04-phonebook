c742b0c59db13812b23639283c284aeab964b204
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-06-29 08:22:31 +0300

    Update pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,7 @@
 // import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-import PropTypes from 'prop-types';
-import { Form, Label, Input, Button } from './ContactForm.styled';
+import { nanoid } from "nanoid";
+import PropTypes from "prop-types";
+import { Form, Label, Input, Button } from "./ContactForm.styled";
 
 // Refactoring code using React-Hooks
 const ContactForm = ({ onSubmit }) => {
@@ -15,7 +15,7 @@ const ContactForm = ({ onSubmit }) => {
         <Input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           id={nameInputId}
@@ -26,7 +26,7 @@ const ContactForm = ({ onSubmit }) => {
         <Input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           id={numberInputId}
